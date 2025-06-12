47b83efef06cafda98f3a5a6583e52be3c67e4fd
afreen saba <118986989+afreensaba448@users.noreply.github.com>
2025-06-12 18:21:32 +0530

    paragraph has removed

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App=()=>{
   return<>
   <Card>
     <h1 style={style}>my card</h1>
-    <p > this is some content for card 2 whats new  </p>
+    <p > this is some content   </p>
   </Card>
   <Card>
 
