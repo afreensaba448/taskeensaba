9e3ab82df48d006fd29bc6c643e6c382e26260aa
Afreen Saba <afreensaba448@gamil.com>
2025-06-12 18:35:29 +0530

    update card1 pera

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ const App=()=>{
   <Card>
 
     <h1>my card</h1>
-    <p>this is some content for card 1hhjkwsdjewk</p>
+    <p>this is some content for card</p>
   </Card>
   <Card>
     <h1>my card</h1>
